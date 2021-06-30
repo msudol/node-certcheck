4d6b8512d2eb01e0076240a10e5150955bb0b4cc
msudol <matt.sudol@gmail.com>
2021-06-30 16:00:53 -0400

    looping through multiple targets to get cert info

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
+/**
+ * node-webcrawler - A node.js application to scan ssl/tls certificates and compile their information.
+ * @license GPLV3
+ *
+ *  https://github.com/msudol/node-certcheck
+ */
+ 
+'use strict';
+
+// get config values
+const config = require('./config/config.js');
+
 var https = require('https');
-var options = {
-    host: 'pwn9.com',
-    port: 443,
-    method: 'GET'
-};
+var results = [];
+var completed_requests = 0;
 
-var req = https.request(options, function(res) {
-    //https://nodejs.org/api/tls.html#tls_certificate_object
-    console.log(res.connection.getPeerCertificate());
-});
 
-req.end();
+for (let i = 0; i < config.hosts.length; i++) {
+
+    var req = https.request(config.hosts[i], function(res) {
+        
+        results.push(res.connection.getPeerCertificate());
+        
+        res.on('data', function(){    
+            if (completed_requests++ == config.hosts.length - 1) {
+                console.log(results);
+            }      
+        });
+    });
+
+    req.on('error', (e) => {
+      console.error(e);
+    });
+    
+    req.end();    
+}
+
+
---
+++ config/config.js
@@ -0,0 +1,23 @@
+// config.js
+
+'use strict';
+
+// Static config, do not edit
+let config = {};
+
+config.hosts = [
+    {
+        host: 'pwn9.com',
+        port: 443
+    },
+    {
+        host: 'msudol.com',
+        port: 443
+    },
+    {
+        host: 'www.google.com',
+        port: 443
+    },    
+];
+
+module.exports = config;
